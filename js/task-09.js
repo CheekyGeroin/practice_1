8b747be9f7f0a6fc6999a285251fc710e367da50
Danylo Altukhov <dan.97198@gmail.com>
2023-07-19 14:37:41 +0300

    done last task

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+
+const btn = document.querySelector(".change-color");
+
+const text = document.querySelector(".color");
+
+const changeColor = () => {
+  const newColor = getRandomHexColor();
+
+  body.style.backgroundColor = newColor;
+  text.textContent = body.style.backgroundColor;
+};
+
+btn.addEventListener("click", changeColor);
